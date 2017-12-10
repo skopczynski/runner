d14e3beb7dacf9d1f6225f55a2abd16725ece1c9
Scott Kopczynski <skopczynski@Scotts-MacBook-Pro-2.local>
2017-12-09 19:06:39 -0800

    add run to db

--- controllers/postHelper.js
+++ controllers/postHelper.js
@@ -1,11 +1,7 @@
 const routes = require('express').Router();
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database : 'runner'
-});
+
+
 module.exports = {
     loginUser(req, res){
         console.log(req.body)
@@ -16,8 +12,28 @@ module.exports = {
         res.redirect('/')
     },
 	save_run(req,res){
-		console.log(req.body);
-		res.redirect('/');
+        const con = mysql.createConnection({
+            host     : 'localhost',
+            user     : 'root',
+            password : 'password',
+            database : 'runner'
+        });
+        con.connect(function(err) {
+            if (err) throw err;
+            console.log("Connected!");
+            var sql = "INSERT INTO run (user_id, mileage, run_date, feeling, description) VALUES (%user_id%, %mileage%, %run_date%, %feeling%, %description%)";
+            var rep = {"%user_id%": "1", "%mileage%": req.body.miles, "%run_date%": "\"" + req.body.date + "\"", "%feeling%": req.body.feeling, "%description%": "\"" + req.body.description + "\""};
+            sql = sql.replace(/%\w+%/g, function(all) {
+                return rep[all] || all;
+             });
+             console.log(sql);
+             con.query(sql, function (err, result) {
+                if (err) throw err;
+                con.end();
+                console.log("1 record inserted");
+            });
+        });
+        res.redirect('/');
     },
     settings(req, res){
         console.log(req.body)
