3e56925b4b0f366d99e55bf02818f5274b38c113
devin <roche.devin@ymail.com>
2017-12-12 13:37:54 -0800

    weekly run graph

--- controllers/helper.js
+++ controllers/helper.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 const mysql = require('mysql');
+const moment = require('moment')
 const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
@@ -10,6 +11,7 @@ module.exports = {
 	home(req, res) {
         if(req.cookies.id === undefined)
             return res.render('login', {title: 'runner'});
+
         var con = mysql.createConnection({
             host: 'localhost',
             user: 'root',
@@ -20,9 +22,20 @@ module.exports = {
         con.connect(function(err) {
             if (err) throw err;
             con.query(sql, function (err, result, fields) {
-                if (err) throw err;
-                res.render('index', {data: result, title: 'runner'});
-                con.end();
+                var trun =result
+                sql = "select cast(run_date as date) as date, sum(mileage) as mileage from run where user_id="+req.cookies.id+" and run_date between date_sub(now(), interval 1 week) and now() group by cast(run_date as date);"
+                console.log(sql)
+                con.query(sql, function (err, result) {
+                    if (err) throw err;
+                    var ro = result.map(r => ({day: moment(r.run_date).day(), distance: r.mileage}))
+                    ro.sort((a, b) => {
+                        return a.day - b.day
+                    })
+
+                    console.log(ro)
+                    res.render('index', {data: trun, week: ro, title: 'runner'});
+                    con.end();
+                });
             });
         });
        
@@ -36,6 +49,7 @@ module.exports = {
 	settings(req, res) {
         if(req.cookies.id === undefined)
             return res.render('login', {title: 'runner'});
+
 		res.render('settings', {title: 'runner'});
 	},
 	history(req, res) {
@@ -79,3 +93,4 @@ module.exports = {
 		res.render('team_advanced', {title:'runner'});
 	}
 };
+
